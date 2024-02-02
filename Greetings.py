2b630c53c515821c6005b2a19c2c1c6663e2eca2
sourjin <137176311+sourjin@users.noreply.github.com>
2024-02-02 15:21:01 +0100

    Greetings.py

---
+++ Greetings.py
@@ -0,0 +1,12 @@
+  if "hello" in greeting:
+            print("$0")
+    elif "h" in greeting:
+            print('$20')
+    else:
+            print('$100')
+
+
+
+if __name__ == "__main__":
+    main()
+    prompt=str(input("Greetings:"))
